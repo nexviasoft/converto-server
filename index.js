7e334eaf0bd2ee9d764e92b3ac8561f36a74625a
nexviasoft <nexviasoft@gmail.com>
2026-03-09 20:47:30 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
+const express = require("express");
+const multer = require("multer");
+const { exec } = require("child_process");
+const fs = require("fs");
 
+const app = express();
+const upload = multer({ dest: "uploads/" });
+
+app.get("/", (req, res) => {
+  res.send("Converto API running");
+});
+
+app.post("/convert/mp3", upload.single("file"), (req, res) => {
+  const input = req.file.path;
+  const output = input + ".mp3";
+
+  exec(`ffmpeg -i ${input} -vn -codec:a libmp3lame ${output}`, (err) => {
+    if (err) {
+      console.log(err);
+      return res.status(500).send("Conversion failed");
+    }
+
+    res.download(output, "output.mp3", () => {
+      fs.unlinkSync(input);
+      fs.unlinkSync(output);
+    });
+  });
+});
+
+const PORT = process.env.PORT || 10000;
+
+app.listen(PORT, () => {
+  console.log("Server running on port", PORT);
+});
